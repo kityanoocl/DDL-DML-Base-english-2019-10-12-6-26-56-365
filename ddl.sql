8f2f1b53fb5fa0cabb12b7e131508c727cb1b21c
Kit Yan <kit.yan@oocl.com>
2019-11-14 18:08:24 +0800

    Added SQL commands in the corresponding .sql files

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+SHOW DATABASES;
 -- Enter a certain database
-
+USE db_name;
 -- Create a database
-
+CREATE Database db_name;
 -- Create the database of the designated character set
-
+CREATE Database db_name character set utf8;
 -- Display the creation information fo the database
-
+SHOW CREATE Database db_name;
 -- Revise the codes of the database
-
+ALTER Database db_name character set utf8;
 -- Delete a database
-
+DROP Database db_name;
 -- **Table level**
 -- Revise table name
-
+SHOW tables;
 -- Revise the field's data type
-
+DESC table_name;
 -- Revise field name
-
+ALTER TABLE table_name CHANGE "Old_Field_Name" "New_Field_Name";
 -- Add field
-
+ALTER TABLE table_name ADD "New_Field" INT(10);
 -- Delete field
-
+ALTER TABLE table_name DROP COLOUMN "Field_to_delete";
 -- Revise the table's storage engine
-
+ALTER TABLE table_name engine="Engine Name";
 -- Delete the table's foreign key restriant
-
+ALTER TABLE table_name drop foreign key fk_key;
 -- Delete a table
+DROP TABLE table_name;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+INSERT INTO student(id, name, age, sex) VALUES(003, 'Kit', 24, 'male');
 -- Revise record
-
+UPDATE student SET NAME = 'Patrick' WHERE id == 001;
 --  Delete record
-
+DELETE FROM student WHERE id == 001;
 -- Search record
+SELECT * FROM student WHERE id == 001;
